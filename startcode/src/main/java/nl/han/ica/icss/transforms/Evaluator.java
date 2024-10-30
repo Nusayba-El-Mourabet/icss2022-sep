959d0f2d838fec28032a65909a7a6156ff1b56ce
Nusayba-El-Mourabet <nsybelmourabet@gmail.com>
2024-10-30 10:11:37 +0100

    Transformer level 0

--- startcode/src/main/java/nl/han/ica/icss/transforms/Evaluator.java
+++ startcode/src/main/java/nl/han/ica/icss/transforms/Evaluator.java
@@ -15,6 +15,7 @@ import java.util.LinkedList;
 public class Evaluator implements Transform {
 
     private IHANLinkedList<HashMap<String, Literal>> variableValues;
+
     //
     public Evaluator() {
         //variableValues = new HANLinkedList<>();
@@ -23,32 +24,34 @@ public class Evaluator implements Transform {
     @Override
     public void apply(AST ast) {
         //variableValues = new HANLinkedList<>();
-        applyStylesheet((Stylesheet)ast.root);
+        applyStylesheet((Stylesheet) ast.root);
 
     }
 
-    private void applyStylesheet(Stylesheet node) {
-        applyStylerule((Stylerule)node.getChildren().get(0));
+    private void applyStylesheet(Stylesheet stylesheet) {
+        applyStylerule((Stylerule) stylesheet.getChildren().get(0));
         //bijv boom van width: 10px + 20px,  met ASTNODE AddOperation met nodes van beide 10px en 20px
         // wegwerken en tot 1 node maken
     }
 
-    private void applyStylerule(Stylerule node) {
-        for (ASTNode child : node.getChildren()){
-            if(child instanceof Declaration){
+    private void applyStylerule(Stylerule stylerule) {
+        for (ASTNode child : stylerule.getChildren()) {
+            if (child instanceof Declaration) {
                 applyDeclaration((Declaration) child);
             }
         }
-
     }
 
-    private void applyDeclaration(Declaration node) {
-        //in Declarartion class hbe een Property en Expression
-        node.expression = evalExpression(node.expression);// om een expressie uit te rekenen
+    private void applyDeclaration(Declaration declaration) {
+        //in Declarartion class hebben we een Property en Expression
+        declaration.expression = evalExpression(declaration.expression);// om een expressie uit te rekenen
     }
 
     private Expression evalExpression(Expression expression) {
-        return null;
+        if (expression instanceof Literal) {
+            return expression;
+        }
+         return expression;
     }
 
 
